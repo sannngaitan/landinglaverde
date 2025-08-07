ddc880119be46411562c79c14591bd775600e13d
Paredez Mateo <paredezmarta46@gmail.com>
2025-08-07 17:36:49 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -38,3 +38,26 @@ faqQuestions.forEach(question => {
         answer.style.display = answer.style.display === 'block' ? 'none' : 'block';
     });
 });
+
+  window.REQUIRED_CODE_ERROR_MESSAGE = 'Elija un código de país';
+  window.LOCALE = 'es';
+  window.EMAIL_INVALID_MESSAGE = window.SMS_INVALID_MESSAGE = "La información que ha proporcionado no es válida. Compruebe el formato del campo e inténtelo de nuevo.";
+
+  window.REQUIRED_ERROR_MESSAGE = "Este campo no puede quedarse vacío. ";
+
+  window.GENERIC_INVALID_MESSAGE = "La información que ha proporcionado no es válida. Compruebe el formato del campo e inténtelo de nuevo.";
+
+
+
+
+  window.translation = {
+    common: {
+      selectedList: '{quantity} lista seleccionada',
+      selectedLists: '{quantity} listas seleccionadas',
+      selectedOption: '{quantity} seleccionado',
+      selectedOptions: '{quantity} seleccionados',
+    }
+  };
+
+  var AUTOHIDE = Boolean(0);
+
